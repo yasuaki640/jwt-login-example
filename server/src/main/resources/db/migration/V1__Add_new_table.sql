1ca7964ce1adc5d92bbfe46349b23a2c602c07ef
yasuaki640 <yasuaki640@gmail.com>
2021-01-19 23:15:18 +0900

    usersテーブルを作成

---
+++ server/src/main/resources/db/migration/V1__Add_new_table.sql
@@ -0,0 +1,11 @@
+create table users
+(
+    id         serial
+        constraint users_pk
+            primary key,
+    login_id   varchar(256) not null,
+    password   bytea        not null,
+    created_at timestamp    not null,
+    updated_at timestamp,
+    deleted_at timestamp
+);
