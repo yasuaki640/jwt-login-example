dd65ec39eac3bebc3a645d8479858ee8d48c94db
yasuaki640 <yasuaki640@gmail.com>
2021-01-20 20:38:47 +0900

    unique を付ける

--- server/src/main/resources/db/migration/V1__Add_new_table.sql
+++ server/src/main/resources/db/migration/V1__Add_new_table.sql
@@ -3,9 +3,9 @@ create table users
     id         serial
         constraint users_pk
             primary key,
-    login_id   varchar(256) not null,
-    password   bytea        not null,
-    created_at timestamp    not null,
+    login_id   varchar(256) unique not null,
+    password   bytea               not null,
+    created_at timestamp           not null,
     updated_at timestamp,
     deleted_at timestamp
 );
