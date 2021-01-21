3797a47b9d810f2682645fd557a617d9b9b8e0cb
yasuaki640 <yasuaki640@gmail.com>
2021-01-21 22:26:09 +0900

    軽微な修正

--- server/src/main/resources/db/migration/V1__create_tabrle_users.sql
+++ server/src/main/resources/db/migration/V1__create_tabrle_users.sql
@@ -1,11 +1,15 @@
-create table users
+BEGIN;
+
+CREATE TABLE users
 (
     id         serial
         constraint users_pk
             primary key,
     login_id   varchar(256) unique not null,
-    password   bytea               not null,
+    password   text                not null,
     created_at timestamp           not null,
     updated_at timestamp,
     deleted_at timestamp
 );
+
+COMMIT;
