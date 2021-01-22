0216e0742e61e87295b1a8fb781616e60fcffaf7
yasuaki640 <yasuaki640@gmail.com>
2021-01-22 22:09:16 +0900

    足し算、引き算、割り算、掛け算であるかを保持するカラムを作る

--- server/src/main/resources/db/migration/V3__create_table_questions.sql
+++ server/src/main/resources/db/migration/V3__create_table_questions.sql
@@ -2,16 +2,20 @@ BEGIN;
 
 CREATE TABLE questions
 (
-    id                serial
+    id                 serial
         constraint questions_pk
             primary key,
-    expression        text      not null,
-    has_carry         bool,
-    has_borrow        bool,
-    has_decimal_point bool,
-    created_at        timestamp not null,
-    updated_at        timestamp,
-    deleted_at        timestamp
+    expression         text      not null,
+    has_add            bool,
+    has_subtraction    bool,
+    has_carry          bool,
+    has_borrow         bool,
+    has_multiplication bool,
+    has_division       bool,
+    has_decimal_point  bool,
+    created_at         timestamp not null,
+    updated_at         timestamp,
+    deleted_at         timestamp
 );
 
 COMMIT;
