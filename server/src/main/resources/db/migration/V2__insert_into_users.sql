52167615ab62df2f6ff5249fe0850a510270db9c
yasuaki640 <yasuaki640@gmail.com>
2021-01-22 22:09:55 +0900

    一応指定してあげる

--- server/src/main/resources/db/migration/V2__insert_into_users.sql
+++ server/src/main/resources/db/migration/V2__insert_into_users.sql
@@ -2,7 +2,7 @@ BEGIN;
 
 CREATE EXTENSION IF NOT EXISTS pgcrypto;
 
-INSERT INTO users
+INSERT INTO users(id, login_id, password, created_at, updated_at, deleted_at)
 VALUES (1, 'yasuaki', crypt('password', gen_salt('bf')), now(), null, null),
        (2, 'taiyaki', crypt('password', gen_salt('bf')), now(), null, null),
        (3, 'takoyaki', crypt('password', gen_salt('bf')), now(), null, null);
