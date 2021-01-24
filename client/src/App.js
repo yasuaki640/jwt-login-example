ba034711a8118c188303fc1227d82efa2d599d09
yasuaki640 <yasuaki640@gmail.com>
2021-01-25 08:47:29 +0900

    仮ルーターを定義

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,59 @@
 import './App.css';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
 
 function App() {
     return (
-        <div className="App">
-            <p>Hello world</p>
-        </div>
+        <>
+            <Router>
+                <div>
+                    <nav>
+                        <ul>
+                            <li>
+                                <Link to="/">Index</Link>
+                            </li>
+                            <li>
+                                <Link to="/login">Login</Link>
+                            </li>
+                            <li>
+                                <Link to="/users">Users</Link>
+                            </li>
+                        </ul>
+                    </nav>
+
+                    {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+                    <Switch>
+                        <Route path="/login">
+                            <Login/>
+                        </Route>
+                        <Route path="/users">
+                            <Users/>
+                        </Route>
+                        <Route path="/">
+                            <Index/>
+                        </Route>
+                    </Switch>
+                </div>
+            </Router>
+        </>
     );
+}
+
+function Index() {
+    return <h2>Index</h2>;
+}
+
+function Login() {
+    return <h2>Login</h2>;
+}
+
+function Users() {
+    return <h2>Users</h2>;
 }
 
 export default App;
