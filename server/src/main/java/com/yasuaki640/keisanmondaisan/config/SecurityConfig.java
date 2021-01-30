c9f651fa031ba0619fe3b92b69f116458d61f3a0
yasuaki640 <yasuaki640@gmail.com>
2021-01-30 20:30:35 +0900

    パスワードエンコード方式を決定

--- server/src/main/java/com/yasuaki640/keisanmondaisan/config/SecurityConfig.java
+++ server/src/main/java/com/yasuaki640/keisanmondaisan/config/SecurityConfig.java
@@ -1,9 +1,15 @@
 package com.yasuaki640.keisanmondaisan.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
