6538790e3e86aaee9545fe0cbe058745e1d6cc30
yasuaki640 <yasuaki640@gmail.com>
2021-01-30 20:07:20 +0900

    spring security 導入

---
+++ server/src/main/java/com/yasuaki640/keisanmondaisan/config/SecurityConfig.java
@@ -0,0 +1,9 @@
+package com.yasuaki640.keisanmondaisan.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+}
