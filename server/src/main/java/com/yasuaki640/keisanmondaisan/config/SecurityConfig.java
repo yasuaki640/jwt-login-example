2adf0a1636b1df7a33bb468bf91ab3399afe7adf
yasuaki640 <yasuaki640@gmail.com>
2021-02-04 22:06:25 +0900

    認証用エンドポイントはすべてのリクエストを受け付ける

--- server/src/main/java/com/yasuaki640/keisanmondaisan/config/SecurityConfig.java
+++ server/src/main/java/com/yasuaki640/keisanmondaisan/config/SecurityConfig.java
@@ -2,14 +2,22 @@ package com.yasuaki640.keisanmondaisan.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests(authorize -> authorize
+                .mvcMatchers("/auth").permitAll());
     }
 }
