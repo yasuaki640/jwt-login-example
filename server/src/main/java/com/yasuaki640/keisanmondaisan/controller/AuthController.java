81f0f9180a283517b07699758b3d9c623097e163
yasuaki640 <yasuaki640@gmail.com>
2021-02-06 20:21:59 +0900

    add auth controller template

---
+++ server/src/main/java/com/yasuaki640/keisanmondaisan/controller/AuthController.java
@@ -0,0 +1,7 @@
+package com.yasuaki640.keisanmondaisan.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthController {
+}
---
+++ server/src/test/java/com/yasuaki640/keisanmondaisan/AuthControllerTest.java
@@ -0,0 +1,12 @@
+package com.yasuaki640.keisanmondaisan;
+
+import com.yasuaki640.keisanmondaisan.controller.AuthController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class AuthControllerTest {
+
+    @Autowired
+    AuthController controller;
+}
