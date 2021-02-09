27013ed56b3031ba02fddad996540397d7f29278
yasuaki640 <yasuaki640@gmail.com>
2021-02-10 08:17:01 +0900

    テストのひな型生成

---
+++ server/src/main/java/com/yasuaki640/keisanmondaisan/controller/UserController.java
@@ -0,0 +1,18 @@
+package com.yasuaki640.keisanmondaisan.controller;
+
+import com.yasuaki640.keisanmondaisan.model.AuthResult;
+import com.yasuaki640.keisanmondaisan.model.User;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+
+@RestController
+@RequestMapping("/users")
+@Slf4j
+public class UserController {
+
+}
