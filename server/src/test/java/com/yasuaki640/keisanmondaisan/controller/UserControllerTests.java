dedc923c3e49e7be1eba8c99da629f3eeb30d155
yasuaki640 <yasuaki640@gmail.com>
2021-02-09 21:57:24 +0900

    ひな型だけ作成

---
+++ server/src/test/java/com/yasuaki640/keisanmondaisan/controller/UserControllerTests.java
@@ -0,0 +1,8 @@
+package com.yasuaki640.keisanmondaisan.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class UserControllerTests {
+}
