4f7a987e9fb1d35c456cd6feff6e5ae59292b746
yasuaki640 <yasuaki640@gmail.com>
2021-02-10 08:16:51 +0900

    パッケージ名変えるのとuserの操作認可をメソッドレベルでしてい

--- server/src/main/java/com/yasuaki640/keisanmondaisan/security/SecurityConfig.java
+++ server/src/main/java/com/yasuaki640/keisanmondaisan/security/SecurityConfig.java
@@ -1,7 +1,8 @@
-package com.yasuaki640.keisanmondaisan.config;
+package com.yasuaki640.keisanmondaisan.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -18,6 +19,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests(authorize -> authorize
-                .mvcMatchers("/auth").permitAll());
+                .mvcMatchers(HttpMethod.PUT,"/users/**")
+                .authenticated()
+                .mvcMatchers(HttpMethod.DELETE,"/users/**")
+                .authenticated());
     }
 }
