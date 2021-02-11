d9e229f686c22e2fd25918132c8edc4c24617f27
yasuaki640 <yasuaki640@gmail.com>
2021-02-11 21:10:59 +0900

    karitest

--- server/src/test/java/com/yasuaki640/keisanmondaisan/controller/UserControllerTests.java
+++ server/src/test/java/com/yasuaki640/keisanmondaisan/controller/UserControllerTests.java
@@ -1,8 +1,17 @@
 package com.yasuaki640.keisanmondaisan.controller;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
 
-@SpringBootTest
+@WebMvcTest(value = UserController.class)
 public class UserControllerTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void test_findAll() {
+    }
 }
