6b230cae6828387adf4dd4cb13bc2fa95d542463
yasuaki640 <yasuaki640@gmail.com>
2021-02-11 19:57:27 +0900

    認証系の設定をいじる

--- server/src/main/java/com/yasuaki640/keisanmondaisan/security/SecurityConfig.java
+++ server/src/main/java/com/yasuaki640/keisanmondaisan/security/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
@@ -18,20 +19,26 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
+        http
+                .httpBasic().disable()
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+
+                // users
+                .authorizeRequests()
                 .mvcMatchers(HttpMethod.PUT, "/users/**")
                 .authenticated()
                 .mvcMatchers(HttpMethod.DELETE, "/users/**")
                 .authenticated()
                 .and()
 
-                // LOGIN
+                // login
                 .formLogin()
                 .loginProcessingUrl("/login")
                 .permitAll()
                 .usernameParameter("loginId")
-                .passwordParameter("pass")
-                .and();
+                .passwordParameter("pass");
 
     }
 }
