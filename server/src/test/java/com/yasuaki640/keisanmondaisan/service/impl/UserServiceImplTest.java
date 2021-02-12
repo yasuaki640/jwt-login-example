cbbca7160db4e522041c6b19dd4506a7b730efba
yasuaki640 <yasuaki640@gmail.com>
2021-02-12 22:37:47 +0900

    ちまちまと修正

--- server/src/test/java/com/yasuaki640/keisanmondaisan/service/impl/UserServiceImplTest.java
+++ server/src/test/java/com/yasuaki640/keisanmondaisan/service/impl/UserServiceImplTest.java
@@ -1,27 +1,37 @@
 package com.yasuaki640.keisanmondaisan.service.impl;
 
 import com.yasuaki640.keisanmondaisan.config.AppDataSource;
+import com.yasuaki640.keisanmondaisan.repository.UserRepository;
 import org.dbunit.IDatabaseTester;
 import org.dbunit.JdbcDatabaseTester;
+import org.dbunit.dataset.DataSetException;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.net.MalformedURLException;
 
 @SpringBootTest(classes = UserServiceImpl.class)
 class UserServiceImplTest {
 
     private static IDatabaseTester databaseTester;
 
+    @MockBean
+    UserRepository repository;
+
     @BeforeEach
-    void setUp() throws ClassNotFoundException {
+    void setUp() throws ClassNotFoundException, MalformedURLException, DataSetException {
         databaseTester = new JdbcDatabaseTester(
                 AppDataSource.DRIVER,
                 AppDataSource.url,
                 AppDataSource.username,
                 AppDataSource.password
         );
-        
+
     }
 
+    @Test
     void test_findById() {
 
     }
