8c7e56852d4f711062bd818b91e6b71e1e277880
yasuaki640 <yasuaki640@gmail.com>
2021-02-12 23:14:32 +0900

    DBUnitむつかすいね

--- server/src/test/java/com/yasuaki640/keisanmondaisan/service/impl/UserServiceImplTest.java
+++ server/src/test/java/com/yasuaki640/keisanmondaisan/service/impl/UserServiceImplTest.java
@@ -1,16 +1,17 @@
 package com.yasuaki640.keisanmondaisan.service.impl;
 
-import com.yasuaki640.keisanmondaisan.config.AppDataSource;
 import com.yasuaki640.keisanmondaisan.repository.UserRepository;
 import org.dbunit.IDatabaseTester;
 import org.dbunit.JdbcDatabaseTester;
-import org.dbunit.dataset.DataSetException;
+import org.dbunit.dataset.IDataSet;
+import org.dbunit.dataset.xml.FlatXmlDataSetBuilder;
+import org.dbunit.operation.DatabaseOperation;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
-import java.net.MalformedURLException;
+import java.io.File;
 
 @SpringBootTest(classes = UserServiceImpl.class)
 class UserServiceImplTest {
@@ -21,14 +22,21 @@ class UserServiceImplTest {
     UserRepository repository;
 
     @BeforeEach
-    void setUp() throws ClassNotFoundException, MalformedURLException, DataSetException {
+    void setUp() throws Exception {
+
         databaseTester = new JdbcDatabaseTester(
-                AppDataSource.DRIVER,
-                AppDataSource.url,
-                AppDataSource.username,
-                AppDataSource.password
+                "org.postgresql.Driver",
+                "jdbc:postgresql://localhost:5432/keisanmondaisan",
+                "keisanmondaisan",
+                "keisanmondaisan"
         );
 
+        IDataSet dataSet = new FlatXmlDataSetBuilder()
+                .build(new File("./data/After.xml"));
+
+        databaseTester.setDataSet(dataSet);
+        databaseTester.setSetUpOperation(DatabaseOperation.CLEAN_INSERT);
+        databaseTester.onSetup();
     }
 
     @Test
