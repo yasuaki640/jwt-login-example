f20dc440b4edeb7209c771720698d4d2dee6530d
yasuaki640 <yasuaki640@gmail.com>
2021-02-14 19:34:51 +0900

    jwtを発行できるように/authenticateにはすべてのアクセスを許可する

--- src/main/java/com/yasuaki640/jwtloginexample/SecurityConfigure.java
+++ src/main/java/com/yasuaki640/jwtloginexample/SecurityConfigure.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -24,6 +25,20 @@ public class SecurityConfigure extends WebSecurityConfigurerAdapter {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf()
+                .disable()
+
+                .authorizeRequests()
+                .antMatchers("/authenticate")
+                .permitAll()
+
+                .anyRequest()
+                .authenticated();
     }
 
     @Bean
