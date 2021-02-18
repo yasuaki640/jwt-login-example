dfdc10dad298d488df21520d825199db6226364f
yasuaki640 <yasuaki640@gmail.com>
2021-02-18 21:22:07 +0900

    validationをいれかえ

--- src/main/java/com/yasuaki640/jwtloginexample/model/SiteUser.java
+++ src/main/java/com/yasuaki640/jwtloginexample/model/SiteUser.java
@@ -21,13 +21,14 @@ public class SiteUser {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank
     @Size(min = 2, max = 20)
     private String username;
 
+    @NotBlank
     @Size(min = 4, max = 255)
     private String password;
 
-    @NotBlank
     @Email
     private String email;
 
