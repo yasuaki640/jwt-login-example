c8d3e1c3a66466650c172a4b7378974434a2800d
yasuaki640 <yasuaki640@gmail.com>
2021-02-18 21:23:53 +0900

    user認証テスト

--- src/test/java/com/yasuaki640/jwtloginexample/controller/AuthenticateControllerTests.java
+++ src/test/java/com/yasuaki640/jwtloginexample/controller/AuthenticateControllerTests.java
@@ -2,6 +2,10 @@ package com.yasuaki640.jwtloginexample.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.yasuaki640.jwtloginexample.model.AuthenticationRequest;
+import com.yasuaki640.jwtloginexample.model.SiteUser;
+import com.yasuaki640.jwtloginexample.repository.SiteUserRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -23,9 +27,25 @@ public class AuthenticateControllerTests {
     @Autowired
     private ObjectMapper objectMapper;
 
+    @Autowired
+    private SiteUserRepository repository;
+
+    private SiteUser testUser = SiteUser.of("yasu", "pass");
+
+    @BeforeEach
+    void setUp() {
+        repository.save(testUser);
+    }
+
+    @AfterEach
+    void tearDown() {
+        repository.deleteAll();
+    }
+
     @Test
     void test_authenticateSuccess() throws Exception {
-        AuthenticationRequest authenticationRequest = new AuthenticationRequest("yasu", "pass");
+        AuthenticationRequest authenticationRequest = new AuthenticationRequest(
+                testUser.getUsername(), testUser.getPassword());
         String requestJson = objectMapper.writeValueAsString(authenticationRequest);
 
         RequestBuilder builder = MockMvcRequestBuilders
@@ -42,8 +62,25 @@ public class AuthenticateControllerTests {
     }
 
     @Test
-    void test_authenticateFailure() throws Exception {
-        AuthenticationRequest authenticationRequest = new AuthenticationRequest("bad", "wrong");
+    void test_authenticateFailure_wrongPassword() throws Exception {
+        AuthenticationRequest authenticationRequest = new AuthenticationRequest(
+                testUser.getUsername(), testUser.getPassword() + "is wrong");
+        String requestJson = objectMapper.writeValueAsString(authenticationRequest);
+
+        RequestBuilder builder = MockMvcRequestBuilders
+                .post("/authenticate")
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(requestJson);
+
+        mockMvc.perform(builder)
+                .andExpect(status().isForbidden());
+    }
+
+    @Test
+    void test_authenticateFailure_wrongUsername() throws Exception {
+        AuthenticationRequest authenticationRequest = new AuthenticationRequest(
+                testUser.getUsername() + "is wrong", testUser.getPassword());
         String requestJson = objectMapper.writeValueAsString(authenticationRequest);
 
         RequestBuilder builder = MockMvcRequestBuilders
